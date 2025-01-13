040d54c70e7d3710694cd4ca9c023203ecb4cdf2
Léo De Santis <leo.desantispro@gmail.com>
2025-01-13 17:31:06 +0100

    :construction: affichage de la gallery complète des items

---
+++ components/ItemCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/ItemCard.css";
+
+export default function ItemCard({ name, icon }) {
+  const imageUrl = `data:image/png;base64,${icon}`;
+  return (
+    <div className="item-card">
+      <img id="icon" src={imageUrl} alt={name} />
+      <p className="title">{name}</p>
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import React, { useState, useEffect } from "react";
+import ItemCard from "../components/ItemCard";
+import itemsData from "./itemsByName.json"; 
+import "../style/ItemCard.css";
+
+
 export default function App() {
-  return <main>Hello World</main>
+  const [items, setItems] = useState([]); 
+
+  useEffect(() => {
+    const transformedItems = Object.entries(itemsData).map(([key, value]) => ({
+      id: key,
+      name: value.name,
+      icon: value.icon,
+    }));
+    console.log(transformedItems);
+    setItems(transformedItems); 
+  }, []);
+
+  return (
+    <main id="gallery">
+      {items.map((item) => (
+        <ItemCard key={item.id} name={item.name} icon={item.icon} />
+      ))}
+    </main>
+  );
 }
